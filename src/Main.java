d11cb0068e873bf943b5852f6f0a71af96c8a4d5
unknown <joshua310041@gmail.com>
2017-10-16 15:21:30 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String str;
+        str=in.next();
+        System.out.println((char)Integer.parseInt(str,16));
+    }
+}
